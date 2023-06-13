d65a24f4dff25be9b97e486f23d7a08d2e878312
Melina Fuch <sosafuchmelina@gmail.com>
2023-06-13 16:14:21 -0300

    elimino comentario

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,16 +2,12 @@ import styles from "@/styles/LandingPage.module.css";
 import Image from "next/image";
 import Link from "next/link";
 
-//Yo estuve aqui
-
 export default function LandingPage() {
   return (
     <div className={styles.container}>
       <div className={styles.container__div}>
         <div className={styles.button_container}>
-          <button className={styles.button_container__button}>
-            <Link href="/login">INICIAR SESIÓN</Link>
-          </button>
+        <button className={styles.button_container__button}><Link href="/login">INICIAR SESIÓN</Link></button>
         </div>
         <div className={styles.logo_container}>
           <Image
@@ -19,21 +15,16 @@ export default function LandingPage() {
             src="/img/logo.png"
             width={24}
             height={37}
-          />
+            />
         </div>
         <div className={styles.h1_container}>
           <h1 className={styles.h1}>COOKQUEEN</h1>
         </div>
         <div className={styles.h3_container}>
           <h3 className={styles.h3}>
-            ¡Hola! En este sitio encontrarás deliciosas recetas de todo tipo
-            creadas por una madre imaginativa y creativa. Además, puedes
-            contactar a esta talentosa persona para pedirle consejos o encargar
-            alguna comida o receta en particular.
+          ¡Hola! En este sitio encontrarás deliciosas recetas de todo tipo creadas por una madre imaginativa y creativa. Además, puedes contactar a esta talentosa persona para pedirle consejos o encargar alguna comida o receta en particular. 
           </h3>
-          <h3 className={styles.h3}>
-            ¡Disfruta y que tengas una buena cocción!
-          </h3>
+          <h3 className={styles.h3}>¡Disfruta y que tengas una buena cocción!</h3>
         </div>
         <div className={styles.image_container}>
           <Image
@@ -41,15 +32,13 @@ export default function LandingPage() {
             src="/img/landing.jpg"
             width={305}
             height={221}
-            alt="MamiCocina"
+            alt='MamiCocina'
           />
         </div>
         <div className={styles.button_snd_container}>
-          <button className={styles.button_snd}>
-            <Link href="/home">A COCINAR!</Link>
-          </button>
+          <button className={styles.button_snd}><Link href="/home">A COCINAR!</Link></button>
         </div>
       </div>
     </div>
-  );
+  )
 }
