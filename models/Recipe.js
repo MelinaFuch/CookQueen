8dab8d12f5231eb2f438fd2bf89c288dae06d61d
Alucard-P <paulmct93@gmail.com>
2023-06-13 19:59:26 -0500

    Cambios menores en los models

--- models/Recipe.js
+++ models/Recipe.js
@@ -1,41 +1,50 @@
 import mongoose from "mongoose";
 
 const RecipeSchema = new mongoose.Schema({
-    title: {
-        type: String,
-        required: [true, 'Por favor, ingrese un título']
-    },
-    category: {
-        type: String,
-        enum: ['Dulce', 'Salado', 'Vegetariano', 'Pasta', 'Desayuno/Merienda', 'Almuerzo', 'Mascotas'],
-        required: [true, 'Por favor, seleccione al menos una categoría']
-    },
-    ingredients: {
-        type: String,
-        required: [true, 'Por favor, ingrese los ingredientes']
-    },
-    description: {
-        type: String,
-        required: [true, 'Por favor, ingrese una descripción']
-    },
-    image: {
-        type: Buffer,
-        required: [true, 'Por favor, sube una foto de la comida'],
-        default: 'https://c8.alamy.com/compes/2jbxacb/panqueques-de-comida-kawaii-2jbxacb.jpg'
-    },
-    video: {
-        type: String
-    },
-    deleted: {
-        type: String,
-        enum: ['si', 'no']
-    },
-    date: {
-        type: Date,
-        default: Date.now
-    }
-    // FALTARIA LIKES Y COMENTARIOS
+  title: {
+    type: String,
+    required: [true, "Por favor, ingrese un título"],
+  },
+  category: {
+    type: String,
+    enum: [
+      "Dulce",
+      "Salado",
+      "Vegetariano",
+      "Pasta",
+      "Desayuno/Merienda",
+      "Almuerzo",
+      "Mascotas",
+    ],
+    required: [true, "Por favor, seleccione al menos una categoría"],
+  },
+  ingredients: {
+    type: String,
+    required: [true, "Por favor, ingrese los ingredientes"],
+  },
+  description: {
+    type: String,
+    required: [true, "Por favor, ingrese una descripción"],
+  },
+  image: {
+    type: String,
+    required: [true, "Por favor, sube una foto de la comida"],
+    // default:
+    //   "https://c8.alamy.com/compes/2jbxacb/panqueques-de-comida-kawaii-2jbxacb.jpg",
+  },
+  video: {
+    type: String,
+  },
+  deleted: {
+    type: String,
+    enum: ["si", "no"],
+    default: "no",
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  // FALTARIA LIKES Y COMENTARIOS
 });
 
-
-export default mongoose.models.Recipe || mongoose.model('Recipe', RecipeSchema);
+export default mongoose.models.Recipe || mongoose.model("Recipe", RecipeSchema);
--- models/User.js
+++ models/User.js
@@ -1,24 +1,24 @@
 import mongoose from "mongoose";
 
 const RecipeSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    password: {
-        type: String,
-        required: true
-    },
-    image: {
-        type: Buffer,
-        default: 'https://i.pinimg.com/236x/ab/01/43/ab01437a16fdf57072342eb1a9bc303a.jpg'
-    },
-    mail: {
-        type: String,
-        required: true
-    },
-    // FALTARIA FAVORITOS
+  name: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: Buffer,
+    default:
+      "https://i.pinimg.com/236x/ab/01/43/ab01437a16fdf57072342eb1a9bc303a.jpg",
+  },
+  mail: {
+    type: String,
+    required: true,
+  },
+  // FALTARIA FAVORITOS
 });
 
-
-export default mongoose.models.Recipe || mongoose.model('Recipe', RecipeSchema);
+export default mongoose.models.Recipe || mongoose.model("Recipe", RecipeSchema);
