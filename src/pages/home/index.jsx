df0eac1120b7f51239faf6ecebc2ae630e65cb92
Melina Fuch <sosafuchmelina@gmail.com>
2023-06-20 19:26:44 -0300

    modified sliders

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -8,18 +8,20 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import data from "../../data";
+import Recipe from "../../../models/Recipe";
+import conectionDB from "../../../lib/dbConnect";
 
-export default function inicio() {
+export default function inicio({recipess}) {
   const renderRecipes = (allRecipes, cantidad) => {
     return allRecipes.slice(0, cantidad).map((recipe) => {
       return <SliderCard recipe={recipe} key={recipe.id} />;
     });
   };
-  const renderCards = (allRecipes, cantidad) => {
-    return allRecipes.slice(0, cantidad).map((recipe) => {
-      return <Card recipe={recipe} key={recipe.id} />;
-    });
-  };
+  // const renderCards = (allRecipes, cantidad) => {
+  //   return allRecipes.slice(0, cantidad).map((recipe) => {
+  //     return <Card recipe={recipe} key={recipe.id} />;
+  //   });
+  // };
 
   const settings = {
     dots: true,
@@ -29,7 +31,7 @@ export default function inicio() {
     arrows: false,
     slidesToScroll: 1,
   };
-  const recipes = data.results;
+  // const recipes = data.results;
 
   return (
     <div className={styles.container}>
@@ -40,40 +42,47 @@ export default function inicio() {
           width={66}
           height={37}
           alt="logo"
-        />
+          />
       </div>
       <div>
         <Slider className={styles.slider} {...settings}>
-          {renderRecipes(recipes, 5)}
+          {renderRecipes(recipess, 5)}
         </Slider>
       </div>
-      <Card />
+        <Card recipes={recipess} />
       <div>
         <h1 className={styles.title}>¡Novedades!</h1>
-        <Card />
+        <Card recipes={recipess} />
       </div>
       <div>
         <h1 className={styles.title}>¡Para desayunar/merendar rico!</h1>
-        <Card />
+        <Card recipes={recipess} />
       </div>
       <div>
         <h1 className={styles.title}>¡A almorzar!</h1>
-        <Card />
+        <Card recipes={recipess} />
       </div>
     </div>
   );
 }
 
-// export async function getStaticProps() {
-//     try {
-//         const res = await fetch("https://api.spoonacular.com/recipes/complexSearch?apiKey=f3c7301f7c9641a38500086d88e7786d");
-//         const data = await res.json();
-//         return {
-//             props: data
-//         }
-//     } catch (error) {
-//         console.log(error)
-//     }
-// }
+export const getServerSideProps = async () => {
+  try {
+    await conectionDB();
 
-// &number=100&addRecipeInformation=true
+    const res = await Recipe.find();
+    
+    const recipess = res.map(mov => {
+      const recipe= mov.toObject();
+      recipe._id = `${recipe._id}`;
+      recipe.date = `${recipe.date}`;
+      
+      return recipe;
+    })
+
+    return { props: { recipess } };
+  } catch (error) {
+    console.log(error);
+    return { props: {success: false, error: '¡Error!'} };
+  }
+};
