7453feb49d869ca878f9b2123d5d1ca8ba96c67a
Melina Fuch <sosafuchmelina@gmail.com>
2023-06-29 11:11:54 -0300

    modified(classNames)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,48 +1,60 @@
 import styles from "@/styles/LandingPage.module.css";
 import Image from "next/image";
 import Link from "next/link";
+// import { useSelector,useDispatch } from "react-redux";
+import { useGetRecipeQuery } from "../../redux/recipesApi";
 
 export default function LandingPage() {
+  
+  // const count = useSelector(state => state.recipeslice.Recipe);
+  // const dispatch = useDispatch();
+  const { data , error, isLoading, isFetching } = useGetRecipeQuery('64961b2ccf60660cb80a060d');
+  
+  if (isLoading || isFetching) return <h1>Loading...</h1>;
+  if (error) return <div>{console.log(error)}</div>
+
   return (
     <div className={styles.container}>
-      <div className={styles.container__div}>
-        <div className={styles.button_container}>
-          <button className={styles.button_container__button}>
-            <Link href="/login">INICIAR SESIÓN</Link>
-          </button>
-        </div>
-        <div className={styles.logo_container}>
-          <Image
-            className={styles.logo}
-            src="/img/logo.png"
-            width={24}
-            height={37}
-            alt="Logo"
-          />
-        </div>
-        <div className={styles.h1_container}>
-          <h1 className={styles.h1}>COOKQUEEN</h1>
-        </div>
-        <div className={styles.h3_container}>
-          <h3 className={styles.h3}>
+      <div className={styles.sub_container}>
+        <div className={styles.left}>
+          <div className={styles.container__title}>
+            <Image
+              className={styles.logo}
+              src="/img/logo.png"
+              width={24}
+              height={37}
+              alt="Logo"
+            />
+            <h1 className={styles.title}>COOKQUEEN</h1>
+          </div>
+          <h3 className={styles.text}>
             ¡Hola! En este sitio encontrarás deliciosas recetas de todo tipo
             creadas por una madre imaginativa y creativa. Además, puedes
             contactar a esta talentosa persona para pedirle consejos o encargar
             alguna comida o receta en particular.
             ¡Disfruta y que tengas una buena cocción!
           </h3>
-          <Image
-            className={styles.image}
-            src="/img/landing.jpg"
-            width={305}
-            height={221}
-            alt="MamiCocina"
-          />
+          <div className={styles.container__button_aCocinar}>
+            <button className={styles.button_aCocinar}>
+              <Link href="/home">A COCINAR!</Link>
+            </button>
+          </div>
         </div>
-        <div className={styles.button_snd_container}>
-          <button className={styles.button_snd}>
-            <Link href="/home">A COCINAR!</Link>
-          </button>
+        <div className={styles.right}>
+          <div className={styles.container__button_login}>
+            <button className={styles.button_login}>
+              <Link href="/login">INICIAR SESIÓN</Link>
+            </button>
+          </div>
+          <div className={styles.container__image}>
+            <Image
+              className={styles.image}
+              src="/img/landing.jpg"
+              width={305}
+              height={221}
+              alt="MamiCocina"
+            />
+          </div>
         </div>
       </div>
     </div>
