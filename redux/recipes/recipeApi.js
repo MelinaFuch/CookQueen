d440483da4945620d7106d1d2c289cbea3925a78
Melina Fuch <sosafuchmelina@gmail.com>
2023-06-29 12:32:55 -0300

    add(folders api[redux-toolkit])

---
+++ redux/recipes/recipeApi.js
@@ -0,0 +1,52 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const recipeApi = createApi({
+    reducerPath: "recipeApi",
+    baseQuery: fetchBaseQuery({
+        baseUrl:"http://localhost:3000"
+        // process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"
+    }),
+    endpoints: (builder) => ({
+        getAllRecipes: builder.query({
+            query: () => ({
+                url: "/api/recipes",
+            }),
+        }),
+        getRecipe: builder.query({
+            query: (id) => ({
+                url: `/api/recipes/${id}`
+            })
+        }),
+        postRecipe: builder.mutation({
+            query: (recipe) => ({
+                url: '/api/recipes',
+                method: "POST",
+                body: recipe,
+                headers: { "Content-Type": "application/json" },
+            })
+        }),
+        editRecipe: builder.mutation({
+            query: (id, newRecipe) => ({
+                url: `/api/recipes/${id}`,
+                method: "PUT",
+                body: newRecipe,
+                headers: { "Content-Type": "application/json" },
+            })
+        }),
+        deleteRecipe: builder.mutation({
+            query: (id) => ({
+                url: `/api/recipes/${id}`,
+                method: "DELETE",
+                headers: { "Content-Type": "application/json" },
+            }),
+        }),
+    }),
+});
+
+export const {
+    useGetAllRecipesQuery, 
+    useGetRecipeQuery, 
+    usePostRecipeMutation,
+    useEditRecipeMutation,
+    useDeleteRecipeMutation
+} = recipeApi
---
+++ redux/users/userApi.js
@@ -0,0 +1,52 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+export const userApi = createApi({
+    reducerPath: "userApi",
+    baseQuery: fetchBaseQuery({
+        baseUrl:"http://localhost:3000"
+        // process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000"
+    }),
+    endpoints: (builder) => ({
+        getAllUsers: builder.query({
+            query: () => ({
+                url: "/api/users",
+            }),
+        }),
+        getUser: builder.query({
+            query: (id) => ({
+                url: `/api/users/${id}`
+            })
+        }),
+        postUser: builder.mutation({
+            query: (recipe) => ({
+                url: '/api/users',
+                method: "POST",
+                body: recipe,
+                headers: { "Content-Type": "application/json" },
+            })
+        }),
+        editUser: builder.mutation({
+            query: (id, newRecipe) => ({
+                url: `/api/users/${id}`,
+                method: "PUT",
+                body: newRecipe,
+                headers: { "Content-Type": "application/json" },
+            })
+        }),
+        deleteUser: builder.mutation({
+            query: (id) => ({
+                url: `/api/users/${id}`,
+                method: "DELETE",
+                headers: { "Content-Type": "application/json" },
+            }),
+        }),
+    }),
+});
+
+export const {
+    useGetAllUsersQuery, 
+    useGetUserQuery, 
+    usePostUserMutation,
+    useEditUserMutation,
+    useDeleteUserMutation
+} = userApi
