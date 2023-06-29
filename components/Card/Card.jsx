5ee0f2daa0541ced358a382dbe15deb013d7e333
Melina Fuch <sosafuchmelina@gmail.com>
2023-06-29 11:06:15 -0300

    deleted data.js

--- components/Card/Card.jsx
+++ components/Card/Card.jsx
@@ -1,6 +1,5 @@
 // import Link from "next/link";
 import styles from "./Card.module.css";
-import data from "../../src/data";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
